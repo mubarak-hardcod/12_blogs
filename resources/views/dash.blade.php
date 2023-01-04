2f3207a76138e265a4c79ab04d489e8e0f724f0d
mubarak_hardcode <mubarakthajudeenit@gmail.com>
2023-01-04 19:37:30 +0530

    dashboard content

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <main class="signup-form">
     <div class="cotainer">
-       <h4>Welcome To Dashboard!</h4>
+       <h4>Welcome Dashboard!</h4>
     </div>
 </main>
 @endsection
